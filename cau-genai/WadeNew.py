5d5c62e2fd5b6c2a4f60d8b52db68a4a5d376248
WadeWideman <wade.wideman@students.cau.edu>
2025-02-25 13:28:09 -0500

    Update
    Added Lang

--- cau-genai/WadeNew.py
+++ cau-genai/WadeNew.py
@@ -4,7 +4,11 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 languages = {
     "English": "en-US_MichaelV3Voice",
     "Japanese": "ja-JP_EmiV3Voice",
-    "Arabic": "ar-AR_OmarV3Voice"
+    "Arabic": "ar-AR_OmarV3Voice",
+    "Spanish":"es-ES_EnriqueV3Voice",
+    "French":"fr-FR_ReneeV3Voice",
+    "Dutch":"nl-NL_MerelV3Voice",
+    "German": "de-DE_DieterV3Voice"
 }
 
 # Function to synthesize text-to-speech with dynamic input
@@ -32,7 +36,7 @@ def synthesize_speech(text, voice, output_file):
 
 # Input your text and choose the voice
 text_input = input("Enter the text you want to convert to speech: ")
-voice_choice = input("Enter the voice language (e.g., en-US_MichaelV3Voice , ja-JP_EmiV3Voice , ar-AR_OmarV3Voice): ")
+voice_choice = input("Enter the voice language (e.g., en-US_MichaelV3Voice , ja-JP_EmiV3Voice , ar-AR_OmarV3Voice,fr-FR_ReneeV3Voice,es-ES_EnriqueV3Voice,nl-NL_MerelV3Voice,de-DE_DieterV3Voice): ")
 voice_choice = voice_choice.capitalize()
 converted_voice_choice = languages[voice_choice]
 output_filename = 'output_audio.wav'
