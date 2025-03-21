875b568ac111f6b8f233f97ef362f1e7a8dabe23
james jakob whitfield <whitfija@gmail.com>
2025-03-21 13:57:17 -0400

    recording tests

--- cau-genai/dev_scripts/dylan_scripts/Dylan_Smalls.py
+++ cau-genai/dev_scripts/dylan_scripts/Dylan_Smalls.py
@@ -25,7 +25,7 @@ speech_to_text.set_service_url('https://api.au-syd.speech-to-text.watson.cloud.i
 
 
 # send to cloud
-with open(join(dirname(__file__), './.', 'audio-file.flac'),
+with open(join(dirname(__file__), './.', 'Recording.flac'),
                'rb') as audio_file:
     speech_recognition_results = speech_to_text.recognize(
         audio=audio_file,
