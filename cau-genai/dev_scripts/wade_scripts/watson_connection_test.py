6532495db1e29f2d9011d1808616adb3cf93620b
james jakob whitfield <whitfija@gmail.com>
2025-03-27 13:40:42 -0400

    test code

---
+++ cau-genai/dev_scripts/wade_scripts/watson_connection_test.py
@@ -0,0 +1,26 @@
+from ibm_watsonx_ai import APIClient
+from ibm_watsonx_ai import Credentials
+from ibm_watsonx_ai.foundation_models import Model
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+credentials = Credentials(
+                   url = "https://us-south.ml.cloud.ibm.com",
+                   api_key = "[SECRET]"
+                  )
+
+model_id = "ibm/granite-20b-multilingual"  # Choose a model (e.g., Granite, Flan, Mistral)
+
+# Initialize model
+model = Model(
+    model_id=model_id,
+    credentials=credentials,
+    project_id="[SECRET]"  # Found in IBM Cloud
+)
+
+# Simple request-response test
+prompt = "Hello my name is Wade, what can you do"
+response = model.generate_text(prompt)
+
+print(response)
+
+#print("Response:", response["results"][0]["generated_text"])
