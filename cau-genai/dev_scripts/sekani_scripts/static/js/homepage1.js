1b8715a71282c376d8f63e0283c077e8e26485b2
Sekani Burke <kani.burke2017@gmail.com>
2025-04-02 18:41:18 -0400

    Created a test html document to explore three.js components and libraries. Waiting to show group before pushing to main project.

--- cau-genai/dev_scripts/sekani_scripts/static/js/homepage1.js
+++ cau-genai/dev_scripts/sekani_scripts/static/js/homepage1.js
@@ -14,11 +14,11 @@ camera.position.setZ(30);
 // Add OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// Torus setup
+// Torus setup - 10,3,16,100
 const geometry = new THREE.BoxGeometry(10,10,10);
-const material = new THREE.MeshBasicMaterial({
-    color: 0xFF6347,
-    wireframe: true
+const material = new THREE.MeshStandardMaterial({
+    color: 0x3e947e,
+    // wireframe: true
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
@@ -26,24 +26,44 @@ scene.add(cube);
 // Lighting
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
-scene.add(pointLight);
+// scene.add(pointLight);
 
 // Ambient light for even illumination
-const ambientLight = new THREE.AmbientLight(0x404040);
-scene.add(ambientLight);
+const ambientLight = new THREE.DirectionalLight(0xffffff);
+scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200,50);
-scene.add(lightHelper);
+const gridHelper = new THREE.GridHelper(100,50);
+// scene.add(gridHelper)
+
+// cube.position.z=30;
+// cube.position.setX(0)
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+
+    cube.rotation.x += 5.05;
+    cube.rotation.y += 5.075;
+    cube.rotation.z += 5.05;
+
+
+
+
+    camera.position.z = t * -0.055;
+    camera.position.y = t * -0.055;
+    camera.position.x = t * -0.055;
+}
+
+document.body.onscroll = moveCamera()
 
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
-    
+
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.005;
     cube.rotation.z += 0.01;
-    
+
     controls.update();
     renderer.render(scene, camera);
 }
@@ -51,18 +71,21 @@ animate();
 
 //  Add stars
 function addStar(){
-    const geometry = new THREE.Geometry(0.25, 24, 24);
+    const geometry = new THREE.SphereGeometry(0.15, 24, 24);
     const material = new THREE.MeshStandardMaterial( {color: 0xffffff})
     const star = new THREE.Mesh(geometry, material);
 
-    const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+    const[x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
 
     star.position.set(x, y, z);
     scene.add(star);
 }
 
-Array(200).fill.forEach(addStar);
+Array(750).fill().forEach(addStar);
 
+
+const spaceTexture = new THREE.TextureLoader().load('/cau-genai/dev_scripts/sekani_scripts/static/img/bkg.jpg')
+scene.background = spaceTexture;
 // Handle window resizing
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
